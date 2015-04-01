6726622caa878290231850c448a5567f4b6b1ea2
aosiname <osinamea@gmail.com>
2015-04-01 15:05:45 +0100

    trying to get button to rpopulate data from MVC controller

---
+++ AngularMvcDemo/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Optimization;
+
+namespace AngularMvcDemo
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/Libraries/angular.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/Scripts/App/Controllers/CompanionshipController.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ AngularMvcDemo/Scripts/App/Controllers/CompanionshipController.js
@@ -0,0 +1,29 @@
+﻿"use strict";
+
+angular.module("hobbitModule").controller("CompanionshipController", ["$scope", "companionship", '$http', '$templateCache', function ($scope, companionship, $http, $templateCache) {
+    $scope.companions = companionship;
+    $scope.method = 'GET';
+    $scope.url = '/Angular/InitialData2';
+
+
+    $scope.ClearCompanions = function () {
+        $scope.companions = "";
+    };
+
+    $scope.RepopulateCompanions = function () {
+        $scope.code = null;
+        $scope.response = null;
+
+        $http({ method: $scope.method, url: $scope.url, cache: $templateCache }).
+          success(function (data, status) {
+              $scope.status = status;
+              $scope.data = data;
+          }).
+          error(function (data, status) {
+              $scope.data = data || "Request failed";
+              $scope.status = status;
+          });
+    };
+
+
+}]);
